7aa2b632d00bab8cd7ba6f4c533fceff4eaad963
hendrikBischoping <h.bischoping@gmx.de>
2025-01-12 13:18:54 +0100

    ts-basics commited

---
+++ src/script.ts
@@ -0,0 +1,12 @@
+function greet(name: string) {
+    console.log("hello " + name);
+}
+
+greet("Hendrik"); //    funktioniert, weil "Hendrik" ein string ist
+//  greet(3);     funktioniert NICHT, weil 3 eine numer ist (siehe typisierung in Funktion)
+
+//  ACHTUNG: Typisierungs-Fehler werden beim Kompilieren zwar angezeigt, aber "fehlerlos" vom JavaScript ausgeführt
+
+//  Typisierungen fördern die Vermeidung von unerkannten Fehler, da die Übergabe falscher Parameter-Typen von Anfang an unterbunden wird
+//  Typisierungs-Fehler werden unmittelbar im Code, sowie später beim Kompilieren durch "tsc script.ts" aufgezeigt!
+//  Typisierungen sind immer optional
